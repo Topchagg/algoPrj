f80b75e80b15e659cacf2e6bf882ea733942b980
Topchagg <zahar.meednov@gmail.com>
2024-09-23 14:51:42 +0200

    Added tasks

--- findTimeCut.py
+++ findTimeCut.py
@@ -5,24 +5,28 @@ from addCutsToList import *
 from TimeStrIntoTimeDelta import *
 
 def findTimeCut(timeLines, approximateCleanTime):
-    possibleCuts = []
-    possibleEarlest = timedelta(hours=6, minutes=0)
-    possibleLatest = timedelta(hours=22, minutes=0)
+    possibleCuts = [] # Тут сохраняются нужные временные отрезки которые могут одойти заказчику
+    possibleEarlest = timedelta(hours=6, minutes=0) # Самый ранний заказ в 6 утра
+    possibleLatest = timedelta(hours=22, minutes=0) # Самый поздний до 10 вечера
 
 
-    for listOfCuts in timeLines:
-        for cut in listOfCuts:
-            operateOrderTimeStart = timeStrIntoTimeDelta(cut["orderTimeStart"])
+    # listOfCuts -> Это массив временных отрезков работы клинера
+    for listOfCuts in timeLines: 
+        for cut in listOfCuts: # cut представляет из себя время начала и конца работы
+            operateOrderTimeStart = timeStrIntoTimeDelta(cut["orderTimeStart"]) # Время модифицруется в нужный объект который можно сравнивать < = >
 
 
-            operateOrderTimeEnd = timeStrIntoTimeDelta(cut["orderTimeEnd"])
+            operateOrderTimeEnd = timeStrIntoTimeDelta(cut["orderTimeEnd"]) # Время модифицруется в нужный объект который можно сравнивать < = >
 
-            possibleEarlestTaskStart = operateOrderTimeStart - approximateCleanTime
+
+            # Находит ПСЕВДО ближайшее время путём отступов от границы в лево т.е - и право т.е + 
+            possibleEarlestTaskStart = operateOrderTimeStart - approximateCleanTime 
             possibleEarlestTaskEnd = possibleEarlestTaskStart + approximateCleanTime
 
             possibleLatestTaskStart = operateOrderTimeEnd + timedelta(hours=1)
             possibleLatestTaskEnd = possibleLatestTaskStart + approximateCleanTime
 
+            # Сравнивает диапазоны и если диапазоны подходят то добавляет время, сначала проверяется левая граница потом правая
             if possibleEarlest <= possibleEarlestTaskStart < possibleLatest:
                 addCutsToList(listOfCuts, possibleCuts,[possibleEarlestTaskStart, possibleEarlestTaskEnd])
 
--- mainAlgo.py
+++ mainAlgo.py
@@ -4,14 +4,34 @@ from findTimeCut import findTimeCut
 from timeDeltaIntoHumanize import timeDeltaIntoHumanize
 from TimeStrIntoTimeDelta import timeStrIntoTimeDelta
 
-cleaners = [
+
+# Таски
+
+# 1. У каждого уборщика есть выходные это будет массив в котором будет 1 или 3 элемента
+
+FreeDays = ["monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+# 1.1 Соответственно такой клинер не сможет получить заказ на свой выходной (Соответственно функция так же будет принимать в себя нужные данные dayOfOrder)
+
+# 2. Оптимизировать сам алгоритм поиска, тут есть некоторые моменты, к примеру не нужно продолжать собирать timeLines если есть хотя бы один клинер
+# в cleanerThatCanClean[] и нужно будет уже проверять ТОЛЬКО на совпадение или несовпадение границ 
+
+# 3. Обновить mock-данные соответственно добавить каждому юзеру рандомные выходные 
+
+# 4. Псевдо поиск помогает в 70-80% случаях найти нужные тайминги для заказа, но бывает такое, что тайминги складываются очень неудачно, и для этого нужно сделать реальный поиск
+# Этот поиск должен сохранить DurationOfWork + 1 hr и пройти ВСЕ свободные таймланы работника в которые помещяются DurationOfWork + 1 hr, и если находит 5 шт то возвращает преждевременно этот массив
+# Если не находит вообще, то возвращает False (Сделай это отдельным алгоритмом т.к это будет вызываться со стороны клиента)
+
+
+
+cleaners = [ #mockData - Можешь изменять в целом
     {
         "ID":"1",
         "orders": [
             {
                 "id":1,
                 "timeStart": "09:00",
-                "possibleStartNew": "13:00"
+                "possibleStartNew": "13:00" # possibleStartNew - это timeStart + durationOfWork + 1 hour
             },
             {
                 "id":2,
@@ -43,36 +63,37 @@ cleaners = [
 ]
 
 
+# Эта функция нужна для нахождения из множества уборщиков - свободных на именно определенное время
+# Если нет свободных уборщиков - тогда этот алгоритм ищет ПСЕВДО ближайшее свободное время к запросу
+def canOrderOnThisTime(cleaners, timeStartCleaningObject, timeEndCleaningObject,approximateCleanTime): 
+    if timeEndCleaningObject > timeStartCleaningObject: # Проверяет время конца больше за время начала т.е 16:10 > 14:20 к примеру
+        cleanerThatCanClean = [] # Здесь собираются уборщики, которые могут выйти работать на это время
+        globalTimes = [] # Здесь собираются отрезки работы уборщиков (У которых уже есть заказы)
 
-def canOrderOnThisTime(cleaners, timeStartCleaningObject, timeEndCleaningObject,approximateCleanTime):
-    if timeEndCleaningObject > timeStartCleaningObject:
-        cleanerThatCanClean = []
-        globalTimes = []
-
-        for cleaner in cleaners:
-            localTimes = []
-            isPossibleToBookThisCleaner = True
+        for cleaner in cleaners: # Перебор массива уборщиков
+            localTimes = [] # Создаётся массив, который собирает инфу именно ОПРЕДЕЛЕННОГО уборщика
+            isPossibleToBookThisCleaner = True # По дефолту он может принять заказ
 
             for order in cleaner["orders"]:
-                if len(cleaner["orders"]) < 3:
-                    orderTimeStart = timeStrIntoTimeDelta(order["timeStart"])
-                    orderTimeEnd = timeStrIntoTimeDelta(order["possibleStartNew"])
+                if len(cleaner["orders"]) < 3: # Заказы могут принимать уборщики у которых 0-1-2 заказа
+                    orderTimeStart = timeStrIntoTimeDelta(order["timeStart"]) # Время модифицруется в нужный объект который можно сравнивать < = >
+                    orderTimeEnd = timeStrIntoTimeDelta(order["possibleStartNew"]) # Время модифицруется в нужный объект который можно сравнивать < = >
 
-                    orderTimings = {"orderTimeStart": order['timeStart'], "orderTimeEnd": order['possibleStartNew']}
-                    localTimes.append(orderTimings)
+                    orderTimings = {"orderTimeStart": order['timeStart'], "orderTimeEnd": order['possibleStartNew']} # сохраняются тайминги заказа
+                    localTimes.append(orderTimings) # Добавляется время одного из заказов (В будущем это нужно для поиска ближайшего свободного времени)
 
-                    if (orderTimeStart < timeEndCleaningObject and timeStartCleaningObject < orderTimeEnd):
-                        isPossibleToBookThisCleaner = False
+                    if (orderTimeStart < timeEndCleaningObject and timeStartCleaningObject < orderTimeEnd): # Если один из его предыдущих заказов пересекается с новым - тогда этот уборщик не может принять заказ
+                        isPossibleToBookThisCleaner = False # Соответственно - false
                 else:
-                    isPossibleToBookThisCleaner = False
+                    isPossibleToBookThisCleaner = False  # Если у уборщика >= 3 заказов, он не может принять заказ, соответственно = False
 
-            if isPossibleToBookThisCleaner:
-                cleanerThatCanClean.append(cleaner["ID"])
-            globalTimes.append(localTimes)
+            if isPossibleToBookThisCleaner: # Если после цикла переменная всё ещё True значит этот уборщик может принять заказ
+                cleanerThatCanClean.append(cleaner["ID"]) # Добавление ID нужного клинера
+            globalTimes.append(localTimes) # Добавление времена заказа клинера в глобальные TimeCuts (Каждый массив отвечает за опредленного клинера)
 
-        if len(cleanerThatCanClean) > 0:
+        if len(cleanerThatCanClean) > 0: # Если есть хоть один клинер который может принять заказ он возвращается
             return cleanerThatCanClean
-        else:
+        else: # Иначе находятся timeCuts которые показывают на какое время можно заказать уборку
             result = findTimeCut(globalTimes, approximateCleanTime)
             return result
     else:
@@ -81,13 +102,11 @@ def canOrderOnThisTime(cleaners, timeStartCleaningObject, timeEndCleaningObject,
 
 if __name__ == "__main__":
 
-    timeStart = timedelta(hours=14,minutes=0)
-    approximateTimeWork = timedelta(hours=6)
-    approximateTimeEnd = timeStart+approximateTimeWork
+    timeStart = timedelta(hours=14,minutes=0) # Инициализация время начала уборки
+    approximateTimeWork = timedelta(hours=6) # Инициализация предположительной длительности уборки
+    approximateTimeEnd = timeStart+approximateTimeWork # Инициализация предположительного конца работы
 
     result = canOrderOnThisTime(cleaners, timeStart, approximateTimeEnd,approximateTimeWork)
 
     print(result)
 
-    # Проверить оставшиеся исключения
-    # Есть небольшая недоработка с границами, нужно брать +1 час к timeEnd
